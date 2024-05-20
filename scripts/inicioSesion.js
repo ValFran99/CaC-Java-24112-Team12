fe33c07168874b51fb69e74b9dadf1be271fc8d2
ValFran99 <francovalfre999@gmail.com>
2024-05-20 19:49:33 -0300

    completed login form basic validation

--- scripts/inicioSesion.js
+++ scripts/inicioSesion.js
@@ -5,35 +5,42 @@ formulario.addEventListener("submit", evento => {
   evento.preventDefault();
 
   if(validarFormulario()){
+    console.log("Subiendo data...")
     formulario.submit();
+  } else{
+    console.log("Faltan campos por completar...")
   }
 
 });
+
+formulario.querySelectorAll(".inputBox").forEach(input => {
+  input.addEventListener("input", () => {
+    formulario.querySelectorAll(".errorText").forEach(campoError => {
+      campoError.innerText = ""
+    })
+    input.classList.remove("inputBoxError")
+  })
+})
 
 function validarFormulario(){
 
   const email = document.getElementById("email");
   const pass = document.getElementById("password");
+  let flag = true;
 
   if(email.value === ""){
     mostrarError(email, "Por favor ingrese una dirección email")
-    return false
+    flag = false;
   }
   if(pass.value === ""){
     mostrarError(pass, "Por favor ingrese una contraseña")
-    return false
+    flag = false
   }
 
+  return flag
 }
 
-//funcion para validar un correo electronico utilizando una expresion regular
-// function validarEmail(email){
-//   const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   return expresionRegular.test(email)
-// }
-
 function mostrarError(campo, mensaje){
-  console.log(campo)
   const parentDiv = campo.parentNode;
 
   const errorText = parentDiv.querySelector(".errorText");
